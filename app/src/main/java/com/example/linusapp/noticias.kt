31c0ea8be8dedb20a16f92f1627f28364cf57361
imayaramota <mayara.souza@bandtec.com.br>
2023-04-04 21:48:31 -0300

    feat: tela de noticias

---
+++ app/src/main/java/com/example/linusapp/noticias.kt
@@ -0,0 +1,11 @@
+package com.example.linusapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class noticias : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_noticias)
+    }
+}

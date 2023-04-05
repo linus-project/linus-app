22b599bdfc294174997e480f19cb8240b29bfb79
gabrielDamas <gabriel.damas@bandtec.com.br>
2023-04-04 21:58:11 -0300

    atualização da tela de abertura

--- app/src/main/java/com/example/linusapp/Onboarding.kt
+++ app/src/main/java/com/example/linusapp/Onboarding.kt
@@ -3,9 +3,9 @@ package com.example.linusapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Login : AppCompatActivity() {
+class Onboarding : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_onboarding)
     }
 }

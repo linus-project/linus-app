30f57071dc79fba50a4fce088d28dce8cf3adf3f
antoniorslima <antonio.lima@bandtec.com.br>
2023-04-11 20:18:38 -0300

    tela distribuicoes por niveis em progresso 30%

---
+++ app/src/main/java/com/example/linusapp/MyAdapter.kt
@@ -0,0 +1,38 @@
+package com.example.linusapp
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.viewpager.widget.PagerAdapter
+
+class MyAdapter(private val context: Context, private val myModelArrayList: ArrayList<MyModel>) : PagerAdapter() {
+    override fun getCount(): Int {
+        return myModelArrayList.size
+    }
+
+    override fun isViewFromObject(view: View, `object`: Any): Boolean {
+        return view == `object`
+    }
+
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+        val view = LayoutInflater.from(context).inflate(R.layout.card_item, container, false)
+
+
+        val model = myModelArrayList[position]
+        val title = model.title
+        val description = model.description
+        val image = model.image
+
+        view.findViewById<ImageView>(R.id.bannerIv)
+        view.findViewById<TextView>(R.id.titleTv)
+        view.findViewById<TextView>(R.id.descriptionTv)
+        view.setOnClickListener{
+            Toast.makeText(context, "$title \n $description", Toast.LENGTH_SHORT).show()
+        }
+
+        return super.instantiateItem(container, position)
+    }
+}

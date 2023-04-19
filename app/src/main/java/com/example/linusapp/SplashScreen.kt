6107959c5621562c489d99c3c5eef9b78188a81e
noisyboy-official <gabriel.osilva@bandtec.com.br>
2023-04-18 21:54:32 -0300

    feature: implementing splash screen

--- app/src/main/java/com/example/linusapp/SplashScreen.kt
+++ app/src/main/java/com/example/linusapp/SplashScreen.kt
@@ -1,11 +1,18 @@
 package com.example.linusapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 
-class MainActivity : AppCompatActivity() {
+class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+    }
+
+    fun goToOnboarding() {
+        val onboarding = Intent(applicationContext, Onboarding::class.java)
     }
 }

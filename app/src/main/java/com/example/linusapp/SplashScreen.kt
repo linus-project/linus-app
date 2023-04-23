c12f17f1c86f78d1bc694133af224166585b4292
noisyboy-official <aguiarogabriel@hotmail.com>
2023-04-23 14:14:22 -0300

    feature: implementing splash screen redirect

--- app/src/main/java/com/example/linusapp/SplashScreen.kt
+++ app/src/main/java/com/example/linusapp/SplashScreen.kt
@@ -3,16 +3,20 @@ package com.example.linusapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.ImageView
+import java.util.Timer
+import kotlin.concurrent.schedule
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        goToOnboarding()
     }
 
     fun goToOnboarding() {
         val onboarding = Intent(applicationContext, Onboarding::class.java)
+        Timer().schedule(2900){
+            startActivity(onboarding)
+        }
     }
 }

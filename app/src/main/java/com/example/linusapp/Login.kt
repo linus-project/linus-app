023ff7ebeedcc6031ad4b2ecedf8c0a5a0ab7d7e
noisyboy-official <aguiarogabriel@hotmail.com>
2023-04-23 23:22:07 -0300

    feature: implementing login integration

--- app/src/main/java/com/example/linusapp/Login.kt
+++ app/src/main/java/com/example/linusapp/Login.kt
@@ -1,11 +1,53 @@
 package com.example.linusapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
+import com.example.linusapp.utils.Api
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonParser
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.RequestBody.Companion.toRequestBody
+import org.json.JSONObject
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+
+    fun login(component: View) {
+        val wrongPassword = findViewById<TextView>(R.id.wrong_password)
+        val service = Api.getUserApi()
+        val jsonObject = JSONObject()
+        jsonObject.put("username", findViewById<TextView>(R.id.username).text.toString())
+        jsonObject.put("password", findViewById<TextView>(R.id.password).text.toString())
+        val jsonObjectString = jsonObject.toString()
+        val requestBody = jsonObjectString.toRequestBody("application/json".toMediaTypeOrNull())
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val response = service.getLogin(requestBody)
+
+            withContext(Dispatchers.Main) {
+                if (response.isSuccessful) {
+                    val gson = GsonBuilder().setPrettyPrinting().create()
+                    val prettyJson = gson.toJson(
+                        JsonParser.parseString(response.body()?.string())
+                    )
+                    val contentHistory = Intent(applicationContext, VistoPorUltimo::class.java)
+                    contentHistory.putExtra("response", prettyJson)
+                    startActivity(contentHistory)
+                } else {
+                    wrongPassword.visibility = View.VISIBLE
+                }
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/linusapp/vo/UserVO.kt
@@ -0,0 +1,8 @@
+package com.example.linusapp.vo
+
+import com.google.gson.annotations.SerializedName
+
+data class UserVO(
+    val username: String,
+    val password: String
+)

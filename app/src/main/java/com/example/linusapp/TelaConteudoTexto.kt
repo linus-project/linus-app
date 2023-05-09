4bc9c9b68da62d376901e6a9251ebcb1d92ee83b
antoniorslima <antonio.lima@bandtec.com.br>
2023-05-08 21:57:43 -0300

    tela conteudo final

---
+++ app/src/main/java/com/example/linusapp/TelaConteudoTexto.kt
@@ -0,0 +1,11 @@
+package com.example.linusapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TelaConteudoTexto : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_conteudo_texto)
+    }
+}
